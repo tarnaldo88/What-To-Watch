4f11e0f9bd8bfcc0a835b4a5c6b345357d00630c
tarnaldo88 <tarnaldo88@gmail.com>
2023-12-21 02:42:15 -0800

    Added header page and next config to allow link

---
+++ disney-clone/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <Link href="/">
+            <Image 
+                src="https://w.wallhaven.cc/full/x6/wallhaven-x6wjkv.png"                
+                className='cursor-pointer'
+                alt="Disney Logo"
+                width={120}
+                height={100}
+            />
+        </Link>
+        <div>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
