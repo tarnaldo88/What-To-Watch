00f6c2ddda0f3b384ec293575c170b667be44436
tarnaldo88 <tarnaldo88@gmail.com>
2023-12-21 16:11:44 -0800

    adding search form

--- disney-clone/components/Header.tsx
+++ disney-clone/components/Header.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import { ThemeToggler } from './ThemeToggler'
 
 function Header() {
   return (
-    <header>
-        <Link href="/">
+    <header className='fixed w-full z-20 top-0 flex items-center justify-between
+      p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25'>
+        <Link className='mr-10' href="/">
             <Image 
                 src="https://w.wallhaven.cc/full/x6/wallhaven-x6wjkv.png"                
                 className='cursor-pointer'
@@ -15,7 +17,7 @@ function Header() {
             />
         </Link>
         <div className='flex space-x-2'>
-          
+         <ThemeToggler/>
         </div>
     </header>
   )
