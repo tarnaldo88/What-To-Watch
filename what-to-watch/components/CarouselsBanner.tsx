6d009fbedfd3bad815e429c06a0824f51cd7abe8
tarnaldo88 <tarnaldo88@gmail.com>
2025-10-05 18:30:11 -0700

    changed gradient for movie banner on homepage

--- what-to-watch/components/CarouselsBanner.tsx
+++ what-to-watch/components/CarouselsBanner.tsx
@@ -31,18 +31,17 @@ function CarouselsBanner({movies}: Props) {
                         key={movie.id}
                         src={getImagePath(movie.backdrop_path, true)}
                         alt=""
-                        width={1920}
                         height={1080}
                     />    
                     <div className='hidden lg:inline absolute mt-0 top-0 
                         pt-40 xl:pt-52 left-0 lg:mt-40 bg-transparent z-20 
-                        h-full w-full bg-gradient-to-r from-gray-900/90 via-transparent
-                        to-transparent p-10 space-y-5'>
+                        h-full w-full p-10 space-y-5' style={{
+                            background: 'linear-gradient(to right, rgba(185, 28, 28, 0.9) 0%, rgba(185, 28, 28, 0.7) 30%, transparent 100%)'
+                        }}>
                         <h2 className='text-5xl font-bold max-w-xl z-50'>
                             {movie.title}
                         </h2>
                         <p className='max-w-xl line-clamp-3'>{movie.overview}</p>
-
                     </div>
                 </div>
             ))}
